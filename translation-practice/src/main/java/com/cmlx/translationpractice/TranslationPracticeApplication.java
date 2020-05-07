0d8eeb13e62dba2d00ac1b86d48c626a535168bd
cmlx <cmlx1218@163.com>
2020-05-07 19:00:18 +0800

    .feat:初始化SpringBoot练习工程

---
+++ translation-practice/src/main/java/com/cmlx/translationpractice/TranslationPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.cmlx.translationpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranslationPracticeApplication.class, args);
+    }
+
+}
---
+++ translation-practice/src/test/java/com/cmlx/translationpractice/TranslationPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmlx.translationpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslationPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
